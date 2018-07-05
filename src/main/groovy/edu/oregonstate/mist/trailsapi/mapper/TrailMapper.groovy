e62ee0a345724f8c7d58626f3745d9cbd2e52b07
[Ian Wilson] <[wilsonia@oregonstate.edu]>
2018-07-05 16:26:16 -0700

    Mapper package

---
+++ src/main/groovy/edu/oregonstate/mist/trailsapi/mapper/TrailMapper.groovy
@@ -0,0 +1,25 @@
+package edu.oregonstate.mist.trailsapi.mapper
+
+import edu.oregonstate.mist.trailsapi.core.Trail
+import org.skife.jdbi.v2.StatementContext
+import org.skife.jdbi.v2.tweak.ResultSetMapper
+import java.sql.ResultSet
+import java.sql.SQLException
+
+public class TrailMapper implements ResultSetMapper<Trail> {
+	public Trail map(int i, ResultSet rs, StatementContext sc) throws SQLException {
+		new Trail (
+			id: rs.getInt('ID'),
+			name: rs.getString('Name'),
+			zipCode: rs.getInt('zipCode'),
+			difficulty: rs.getString('DIFFICULTY'),
+			largeDrop: rs.getBoolean('LARGEDROP'),
+			smallDrop: rs.getBoolean('SMALLDROP'),
+			woodRide: rs.getBoolean('WOODRIDE'),
+			skinny: rs.getBoolean('SKINNY'),
+			largeJump: rs.getBoolean('LARGEJUMP'),
+			smallJump: rs.getBoolean('SMALLJUMP'),
+			gap: rs.getBoolean('GAP')
+		)
+	}
+}
