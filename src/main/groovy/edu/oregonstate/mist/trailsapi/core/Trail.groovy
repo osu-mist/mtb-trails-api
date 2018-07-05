76f1c38b04f20f54d47cbfb06c329eb762a36221
[Ian Wilson] <[wilsonia@oregonstate.edu]>
2018-07-05 15:16:45 -0700

    Core package

---
+++ src/main/groovy/edu/oregonstate/mist/trailsapi/core/Trail.groovy
@@ -0,0 +1,15 @@
+package edu.oregonstate.mist.trailsapi.core
+
+class Trail {
+	Integer id
+	String name
+	Integer zipCode
+	String difficulty
+	Boolean largeDrop
+	Boolean smallDrop
+	Boolean woodRide
+	Boolean skinny
+	Boolean largeJump
+	Boolean smallJump
+	Boolean gap
+}
