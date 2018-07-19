8f8b9802d92f9cd178602cd2b4021035181459da
[Ian Wilson] <[wilsonia@oregonstate.edu]>
2018-07-19 12:40:34 -0700

    Fix whitespace

--- src/main/groovy/edu/oregonstate/mist/trailsapi/resources/TrailResources.groovy
+++ src/main/groovy/edu/oregonstate/mist/trailsapi/resources/TrailResources.groovy
@@ -29,87 +29,109 @@ import org.skife.jdbi.v2.DBI
 @Path("/trails")
 @Produces(MediaType.APPLICATION_JSON)
 public class TrailsResource extends Resource {
-	private final TrailDAO trailDAO
-	private DBI dbi
+    private final TrailDAO trailDAO
+    private DBI dbi
 
-	TrailsResource(TrailDAO trailDAO, DBI dbi) {
-	    this.trailDAO = trailDAO
-	    this.dbi = dbi
-	}
+    TrailsResource(TrailDAO trailDAO, DBI dbi) {
+        this.trailDAO = trailDAO
+        this.dbi = dbi
+    }
 
-	ResourceObject trailResource(Trail trail) {
-	    new ResourceObject(
-			  id: trail.id,
-			  type: 'Trail',
-			  attributes: trail,
-	    )
-	}
+    ResourceObject trailResource(Trail trail) {
+        new ResourceObject(
+              id: trail.id,
+              type: 'Trail',
+              attributes: trail,
+        )
+    }
 
-	ResultObject trailResult(List<Trail> trails) {
-		new ResultObject( data: trails.collect { trail -> trailResource(trail) })
-	}
+    ResultObject trailResult(List<Trail> trails) {
+        new ResultObject( data: trails.collect { trail -> trailResource(trail) })
+    }
 
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	Response postTrail (@Valid ResultObject newResultObject) {
-		Trail trail
-		Response response
-		trail = (Trail)newResultObject.data.attributes
-		if (!trailValidator(trail)) {
-				//required field missing
-				response = badRequest("Required field missing (name, zip code, or difficulty)").build()
-		} else {
-			Boolean conflictingTrails = trailDAO.getConflictingTrails(trail)
-				if (!conflictingTrails) {
-						Integer id = trailDAO.getNextId()
-						trail.id = id
-						trailDAO.postTrail(trail)
-						//trail object created
-						response = created(trailResource(trail)).build()
-				} else {
-					//Trail already exists
-					response = conflict().build()
-				}
-		}
-		response
-	}
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response postTrail (@Valid ResultObject newResultObject) {
+        Trail trail
+        Response response
+        trail = (Trail)newResultObject.data.attributes
+        if (!trailValidator(trail)) {
+                //required field missing
+                response = badRequest("Required field missing (name, zip code, or difficulty)").build()
+        } else {
+            Boolean conflictingTrails = trailDAO.getConflictingTrails(trail)
+                if (!conflictingTrails) {
+                        Integer id = trailDAO.getNextId()
+                        trail.id = id
+                        trailDAO.postTrail(trail)
+                        //trail object created
+                        response = created(trailResource(trail)).build()
+                } else {
+                    //Trail already exists
+                    response = conflict().build()
+                }
+        }
+        response
+    }
 
-	/*************************************************************************************************
-	Function: trailValidator
-	Description: Checks for validity of trail object
-	Input: Trail object that is to be POST or PUT
-	Output: Returns true if name, zip code, and difficulty are not null, and false otherwise
-	*************************************************************************************************/
-	Boolean trailValidator(Trail trail) {
-		trail.name && trail.zipCode && trail.difficulty
-	}
+    /*************************************************************************************************
+    Function: trailValidator
+    Description: Checks for validity of trail object
+    Input: Trail object that is to be POST or PUT
+    Output: Returns true if name, zip code, and difficulty are not null, and false otherwise
+    *************************************************************************************************/
+    Boolean trailValidator(Trail trail) {
+        trail.name && trail.zipCode && trail.difficulty
+    }
 
-	@GET
-	Response getByQuery (@QueryParam("name") String name,
-			   @QueryParam("difficulty") String difficulty,
-			   @QueryParam("mostDifficult") String mostDifficult,
-			   @QueryParam("leastDifficult") String leastDifficult,
-			   @QueryParam("zipCode") Integer zipCode,
-			   @QueryParam("smallDrop") Boolean smallDrop,
-			   @QueryParam("largeDrop") Boolean largeDrop,
-			   @QueryParam("woodRide") Boolean woodRide,
-			   @QueryParam("skinny") Boolean skinny,
-			   @QueryParam("largeJump") Boolean largeJump,
-			   @QueryParam("smallJump") Boolean smallJump,
-			   @QueryParam("gap") Boolean gap) {
-		List<Trail> trails = trailDAO.getTrailByQuery(name, difficulty, mostDifficult, leastDifficult,
-			zipCode, smallDrop,largeDrop, woodRide, skinny, largeJump,smallJump, gap)
-		ok(trailResult(trails)).build()
-	}
+    @GET
+    Response getByQuery (@QueryParam("name") String name,
+                         @QueryParam("difficulty") String difficulty,
+                         @QueryParam("mostDifficult") String mostDifficult,
+                         @QueryParam("leastDifficult") String leastDifficult,
+                         @QueryParam("zipCode") Integer zipCode,
+                         @QueryParam("smallDrop") Boolean smallDrop,
+                         @QueryParam("largeDrop") Boolean largeDrop,
+                         @QueryParam("woodRide") Boolean woodRide,
+                         @QueryParam("skinny") Boolean skinny,
+                         @QueryParam("largeJump") Boolean largeJump,
+                         @QueryParam("smallJump") Boolean smallJump,
+                         @QueryParam("gap") Boolean gap) {
+        List<Trail> trails = trailDAO.getTrailByQuery(name, difficulty, mostDifficult, leastDifficult,
+            zipCode, smallDrop,largeDrop, woodRide, skinny, largeJump,smallJump, gap)
+        ok(trailResult(trails)).build()
+    }
 
     @GET
     @Path ('/{id: \\d+}')
-	Response getByID (@PathParam('id') Integer id) {
-		Trail trail = trailDAO.getTrailByID(id)
-		if (trail) {
-			ok(trailResource(trail)).build()
-		} else {
-			notFound().build()
-		}
-	}
+    Response getByID (@PathParam('id') Integer id) {
+        Trail trail = trailDAO.getTrailByID(id)
+        if (trail) {
+            ok(trailResource(trail)).build()
+        } else {
+            notFound().build()
+        }
+    }
+
+    @PUT
+    @Path ('/{id: \\d+}')
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response putTrail (@PathParam('id') Integer id, @Valid ResultObject newResultObject) {
+        Trail currentTrail = trailDAO.getTrailByID(id)
+        if (currentTrail) {
+            if (newResultObject) {
+                Trail trail = (Trail)newResultObject.data.attributes
+                trailDAO.updateTrail(id, trail.name, trail.difficulty, trail.zipCode, trail.smallDrop,
+                trail.largeDrop, trail.woodRide, trail.skinny, trail.largeJump, trail.smallJump, trail.gap)
+                //Trail has been updated
+                ok(trail).build()
+            } else {
+                //Body data is missing
+                badRequest("No data in body to PUT").build()
+            }
+        } else {
+            //No trail at ID exists
+            return notFound().build()
+        }
+    }
 }
