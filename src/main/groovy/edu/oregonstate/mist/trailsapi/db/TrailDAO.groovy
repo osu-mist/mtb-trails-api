2c7eabac89ac716452368301bb5bd1fb86fb2817
[Ian Wilson] <[wilsonia@oregonstate.edu]>
2018-07-19 15:10:26 -0700

    correct formatting

--- src/main/groovy/edu/oregonstate/mist/trailsapi/db/TrailDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/trailsapi/db/TrailDAO.groovy
@@ -15,23 +15,23 @@ interface TrailDAO extends Closeable {
     POST /trails
     ***********************************************************************************************/
     @SqlUpdate ("""
-        INSERT INTO TRAILS (ID, NAME, ZIPCODE, DIFFICULTY_ID, POLYLINE, LARGEDROP, SMALLDROP,
-            WOODRIDE, SKINNY, LARGEJUMP, SMALLJUMP, GAP)
-        VALUES (
-            (:trail.id),
-            (:trail.name),
-            (:trail.zipCode),
-            (SELECT DIFFICULTY_ID FROM TRAIL_DIFFICULTIES
-                WHERE DIFFICULTY_COLOR = :trail.difficulty),
-            (:trail.polyline),
-            (:trail.largeDrop),
-            (:trail.smallDrop),
-            (:trail.woodRide),
-            (:trail.skinny),
-            (:trail.largeJump),
-            (:trail.smallJump),
-            (:trail.gap)
-        )
+    INSERT INTO TRAILS (ID, NAME, ZIPCODE, DIFFICULTY_ID, POLYLINE, LARGEDROP, SMALLDROP,
+        WOODRIDE, SKINNY, LARGEJUMP, SMALLJUMP, GAP)
+    VALUES (
+        (:trail.id),
+        (:trail.name),
+        (:trail.zipCode),
+        (SELECT DIFFICULTY_ID FROM TRAIL_DIFFICULTIES
+            WHERE DIFFICULTY_COLOR = :trail.difficulty),
+        (:trail.polyline),
+        (:trail.largeDrop),
+        (:trail.smallDrop),
+        (:trail.woodRide),
+        (:trail.skinny),
+        (:trail.largeJump),
+        (:trail.smallJump),
+        (:trail.gap)
+    )
     """)
     void postTrail(@BindBean("trail") Trail trail)
 
@@ -40,18 +40,19 @@ interface TrailDAO extends Closeable {
     """)
     Integer getNextId()
 
-    @SqlQuery("""SELECT ID FROM TRAILS WHERE
-        NAME = :trail.name
-        AND ZIPCODE = :trail.zipCode
-        AND DIFFICULTY_ID = (SELECT DIFFICULTY_ID FROM TRAIL_DIFFICULTIES
-            WHERE DIFFICULTY_COLOR = :trail.difficulty)
+    @SqlQuery("""
+        SELECT ID FROM TRAILS WHERE
+            NAME = :trail.name
+            AND ZIPCODE = :trail.zipCode
+            AND DIFFICULTY_ID = (SELECT DIFFICULTY_ID FROM TRAIL_DIFFICULTIES
+                WHERE DIFFICULTY_COLOR = :trail.difficulty)
     """)
     List <Integer> getConflictingTrails(@BindBean("trail") Trail trail)
 
     /**********************************************************************************************
     Function: getConflictingTrails
-    Description: Checks that no trails sharing a name, zip code, and difficulty exist in the
-        database.
+    Description: Checks that no trails sharing a name, zip code, and difficulty exist in
+        the database.
     Names are compared case insensitive after removing single quotes and spaces.
     Input: Trail object that is to be checked for conflict
     Output: Returns true if at least one conflict exists, false otherwise
@@ -83,11 +84,11 @@ interface TrailDAO extends Closeable {
             TRAILS.LARGEJUMP,
             TRAILS.SMALLJUMP,
             TRAILS.GAP
-         FROM TRAILS
-         LEFT JOIN TRAIL_DIFFICULTIES ON TRAILS.DIFFICULTY_ID = TRAIL_DIFFICULTIES.DIFFICULTY_ID
+        FROM TRAILS
+        LEFT JOIN TRAIL_DIFFICULTIES ON TRAILS.DIFFICULTY_ID = TRAIL_DIFFICULTIES.DIFFICULTY_ID
             WHERE (UPPER(REPLACE(REPLACE(TRAILS.NAME, ' ', ''), '''', ''))
                 = UPPER(REPLACE(REPLACE(:name, ' ', ''), '''', '')) OR :name IS NULL)
-            AND (UPPER(TRAIL_DIFFICULTIES.DIFFICULTY_COLOR) = UPPER(:difficulty)
+     	    AND (UPPER(TRAIL_DIFFICULTIES.DIFFICULTY_COLOR) = UPPER(:difficulty)
                 OR :difficulty IS NULL)
             AND (TRAILS.DIFFICULTY_ID <= (SELECT DIFFICULTY_ID FROM TRAIL_DIFFICULTIES WHERE
                 UPPER(DIFFICULTY_COLOR) = UPPER(:mostDifficult)) OR :mostDifficult IS NULL)
@@ -101,19 +102,19 @@ interface TrailDAO extends Closeable {
             AND (TRAILS.LARGEJUMP = :largeJump OR :largeJump IS NULL)
             AND (TRAILS.SMALLJUMP = :smallJump OR :smallJump IS NULL)
             AND (TRAILS.GAP = :gap OR :gap IS NULL)
-        """)
-        List<Trail> getTrailByQuery(@Bind("name") String name,
-                                    @Bind("difficulty") String difficulty,
-                                    @Bind("mostDifficult") String mostDifficult,
-                                    @Bind("leastDifficult") String leastDifficult,
-                                    @Bind("zipCode") Integer zipCode,
-                                    @Bind("smallDrop") Boolean smallDrop,
-                                    @Bind("largeDrop") Boolean largeDrop,
-                                    @Bind("woodRide") Boolean woodRide,
-                                    @Bind("skinny") Boolean skinny,
-                                    @Bind("largeJump") Boolean largeJump,
-                                    @Bind("smallJump") Boolean smallJump,
-                                    @Bind("gap") Boolean gap)
+    """)
+    List<Trail> getTrailByQuery(@Bind("name") String name,
+                                @Bind("difficulty") String difficulty,
+                                @Bind("mostDifficult") String mostDifficult,
+                                @Bind("leastDifficult") String leastDifficult,
+                                @Bind("zipCode") Integer zipCode,
+                                @Bind("smallDrop") Boolean smallDrop,
+                                @Bind("largeDrop") Boolean largeDrop,
+                                @Bind("woodRide") Boolean woodRide,
+                                @Bind("skinny") Boolean skinny,
+                                @Bind("largeJump") Boolean largeJump,
+                                @Bind("smallJump") Boolean smallJump,
+                                @Bind("gap") Boolean gap)
 
     /***********************************************************************************************
     GET /trails/{trailID}
@@ -132,9 +133,9 @@ interface TrailDAO extends Closeable {
             TRAILS.LARGEJUMP,
             TRAILS.SMALLJUMP,
             TRAILS.GAP
-         FROM TRAILS
-         LEFT JOIN TRAIL_DIFFICULTIES ON TRAILS.DIFFICULTY_ID = TRAIL_DIFFICULTIES.DIFFICULTY_ID
-         WHERE TRAILS.ID = :id
+       FROM TRAILS
+       LEFT JOIN TRAIL_DIFFICULTIES ON TRAILS.DIFFICULTY_ID = TRAIL_DIFFICULTIES.DIFFICULTY_ID
+       WHERE TRAILS.ID = :id
     """)
     Trail getTrailByID(@Bind("id") Integer id)
 
